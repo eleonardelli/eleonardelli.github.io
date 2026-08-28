cf11d469d23e0a8a7de24b3d4e85ca5f5bffc53e
eleonardelli <73229923+eleonardelli@users.noreply.github.com>
2026-08-28 14:43:15 +0000

    Deploying to gh-pages from @ eleonardelli/eleonardelli.github.io@cd045219c634c9ecaf367f3815ce7fd8080552ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "Projects",
+          title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
